d44ed932f49f92fe0228a5f9193ff0b421a1727b
Embbnux Ji <embbnux@embbnux.com>
2021-09-15 15:15:16 +0800

    upgrade: use widgets lib 0.13.2

--- src/brand.js
+++ src/brand.js
@@ -1,8 +1,25 @@
-export default {
+import { createBrandConfig } from '@ringcentral-integration/commons/modules/Brand/createBrandConfig';
+
+export default createBrandConfig({
   id: '1210',
-  brandCode: 'rc',
   code: 'rc',
   name: 'RingCentral',
   appName: 'RingCentral',
-  fullName: 'RingCentral'
-};
+  fullName: 'RingCentral',
+  application: 'RingCentral Widgets',
+  allowRegionSetting: true,
+  callWithJupiter: {
+    default: {
+      link: 'https://app.ringcentral.com/',
+      protocol: 'rcapp://',
+      name: 'RingCentral',
+    },
+  },
+  rcvTeleconference: 'https://v.ringcentral.com/teleconference/',
+  meetingUriReg: {
+    rcm: undefined,
+    rcv: undefined,
+  },
+  spartanProtocol: 'rcmobile://',
+  allowJupiterUniversalLink: true,
+});
